5990e418ee69428094cb60b3e1b674746c004e57
janna <jannakondeth5@gmail.com>
2024-07-03 18:00:04 +0530

    add page for verify email

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,54 @@
 'use client'
 
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 
 export default  function SignupPage() {
+
+  const router=useRouter()
   const [loading,setLoading]=useState(false)
   const [buttonDisabled,setButtonDisabled]=useState(false)
+
+  const [user,setUser]=useState({
+        username:"",
+        email:"",
+        password:""
+  })
+  
+
+  const handleChange=(e:any)=>{
+         setUser({
+          ...user,
+          [e.target.id]:e.target.value})
+  }
+
+  const onSignup=async()=>{
+        try {
+         setLoading(true)
+         const response= await axios.post('/api/users/signup',user)
+         console.log(response.data)
+         router.push("/login")
+     
+        //  setLoading(false)
+
+        } catch (error:any) {
+          console.log("api calling error",error.message)
+          toast.error(error.message)
+        }
+  }
+
+  useEffect(()=>{
+      if(user.username.length >0 && user.email.length >0 && user.password.length >0 ){
+        setButtonDisabled(false)
+      }else{
+        setButtonDisabled(true)
+      }
+  },[user])
+
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
     <h1>{loading ? "Processing" : "Signup"}</h1>
@@ -15,6 +58,9 @@ export default  function SignupPage() {
     className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="username"
         type="text"
+        placeholder='username'
+        value={user.username}
+        onChange={handleChange}
        
         />
     <label htmlFor="email">email</label>
@@ -22,6 +68,9 @@ export default  function SignupPage() {
     className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
+        placeholder='email'
+        value={user.email}
+        onChange={handleChange}
         
         />
     <label htmlFor="password">password</label>
@@ -29,10 +78,12 @@ export default  function SignupPage() {
     className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="password"
-       
+        value={user.password}
+        placeholder='password'
+        onChange={handleChange}
         />
         <button
-       
+        onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
         <Link href="/login">Visit login page</Link>
     </div>
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+'use client'
 
-function page() {
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+
+export default  function VerifyEmailPage() {
+  const [token,setToken]=useState("")
+  const [verified,setVerified]=useState(false)
+  
+
+  useEffect(()=>{
+      const urlToken= window.location.search.split("=")[1]
+      setToken(urlToken || "")
+      
+  },[])
+  
   return (
-    <div>
-        VerifyEmail Page
+    <div className='flex justify-center items-center flex-col min-h-screen gap-6'>
+       <h2 className='text-2xl  '>Verify Email</h2>
+       <p className='bg-red-800 p-4 rounded-lg'>{token?`${token}`:"No token"}</p>
+
+       {verified && (
+        <div>
+        <h2 className='bg-green-600 p-4 mb-4'>Email verified</h2>
+        <Link href={"/login"}>
+           
+            <h2 className='bg-white p-4 rounded-xl text-black text-center'>Login</h2>
+         
+        </Link>
+          
+        </div>
+       )}
     </div>
   )
 }
 
-export default page
